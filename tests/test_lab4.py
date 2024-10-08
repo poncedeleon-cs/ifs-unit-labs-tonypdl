850cc56f09929a60c4aee9a6722a1f01f36e8131
Antonio Ponce de Leon <42123340+tonypdl@users.noreply.github.com>
2024-10-08 13:38:56 -0500

    Update test_lab4.py

--- tests/test_lab4.py
+++ tests/test_lab4.py
@@ -10,11 +10,11 @@ class TestLab4(unittest.TestCase):
     def test_determineQuadrant(self):
         self.assertEqual(determineQuadrant(4, 2).strip(), "Quadrant 1")
         self.assertEqual(determineQuadrant(-3, 7).strip(), "Quadrant 2")
-        self.assertEqual(determineQuadrant(5, 0).strip(), "Quadrant 1")
-        self.assertEqual(determineQuadrant(0, -8).strip(), "Quadrant 4")
+        self.assertEqual(determineQuadrant(5, 0).strip(), "0")
+        self.assertEqual(determineQuadrant(0, -8).strip(), "0")
         self.assertEqual(determineQuadrant(-6, -4).strip(), "Quadrant 3")
-        self.assertEqual(determineQuadrant(0, 9).strip(), "Quadrant 1")
-        self.assertEqual(determineQuadrant(-10, 0).strip(), "Quadrant 2")
+        self.assertEqual(determineQuadrant(0, 9).strip(), "0")
+        self.assertEqual(determineQuadrant(-10, 0).strip(), "0")
         self.assertEqual(determineQuadrant(1, -7).strip(), "Quadrant 4")
 
 
