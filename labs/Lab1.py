cc76af98db49f18d931a8f594b4b70c4eff4de1a
Antonio Ponce de Leon <42123340+tonypdl@users.noreply.github.com>
2024-10-10 13:49:52 +0000

    refreshing

--- labs/Lab1.py
+++ labs/Lab1.py
@@ -5,3 +5,5 @@
 # define a function leapYear() that takes a given year as
 # a parameter and returns true if the given year is a leap
 # year and false if the given year is not a leap year.
+
+def leapYear(year):
